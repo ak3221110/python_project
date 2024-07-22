8950ada73c7e234a75806ba616b56e1567a00b43
Abhishek Kumar <abhishek401796@outlook.com>
2024-07-22 16:02:30 +0530

    first commit

---
+++ project1.py
@@ -0,0 +1,31 @@
+menu={
+    'pizza':50,
+    'coffie':20,
+    'tea':10,
+    'drink':100
+}
+total_price=0
+#greet
+print("welome to owe restrunt, here is owr menu")
+print('''
+pizza:50
+coffie:20
+tea:10
+drink:100
+
+''')
+
+item_1=input("enter a first item which you want:")
+if item_1 in menu:
+    total_price += menu[item_1]
+    print("your item is added ",item_1)
+else:
+    print("sorry please tell me another else")
+q=input('are you want to another item(yes/no)')
+if q =="yes":
+    item_2=input("enter secound item:")
+    total_price +=menu[item_2]
+    print("your item is added",item_1,item_2)
+else:
+    print("ok , thanks")
+print ("your total amounnt is",total_price)
