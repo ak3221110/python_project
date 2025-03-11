f743552e076200fd3bbf138182122402ac7b3e6e
Abhishek Kumar <152717508+ak3221110@users.noreply.github.com>
2025-03-11 09:24:26 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,31 +1,36 @@
-menu={
-    'pizza':50,
-    'coffie':20,
-    'tea':10,
-    'drink':100
+menu = {
+    'pizza': 50,
+    'Pizza': 50,
+    'coffee': 60,
+    'Coffee': 60,
+    'tea': 10,
+    'Tea': 10,
+    'drink': 20,
+    'Drink': 20,
 }
-total_price=0
-#greet
-print("welome to our restaurant, here is our menu")
-print('''
-pizza:50
-coffee:60
-tea:10
-drink:20
 
+total_price = 0
+
+# Greet the user
+print("Welcome to our restaurant, here is our menu:")
+print('''
+Pizza: 50
+Coffee: 60
+Tea: 10
+Drink: 20
 ''')
 
-item_1=input("Enter a first item which you want:")
-if item_1 in menu:
-    total_price += menu[item_1]
-    print("your item is added ",item_1)
-else:
-    print("sorry please tell me another else")
-q=input('are you want to another item(yes/no)')
-if q =="yes":
-    item_2=input("enter secound item:")
-    total_price +=menu[item_2]
-    print("your item is added",item_1,item_2)
-else:
-    print("ok , thanks")
-print ("your total amounnt is",total_price)
+while True:
+    item = input("Enter an item you want (or type 'done' to finish): ")
+    
+    if item.lower() == 'done':
+        break
+    
+    if item in menu:
+        total_price += menu[item]
+        print(f"Your item '{item}' is added.")
+    else:
+        print("Sorry, please tell me another item.")
+
+# Final output
+print("Your total amount is:", total_price)
